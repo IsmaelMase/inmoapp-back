831df32a93051d7eb933236182313830e75c455a
IsmaelMase <ismaelmfb@gmail.com>
2019-07-14 22:52:19 +0200

    Primero

---
+++ User-component/src/main/java/com/users/User/component/UserComponentApplication.java
@@ -0,0 +1,23 @@
+package com.users.User.component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController()
+public class UserComponentApplication {
+
+	@RequestMapping("/home")
+	public String home() {
+		return "Hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserComponentApplication.class, args);
+	}
+
+}
