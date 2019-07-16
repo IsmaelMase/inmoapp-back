bc3eb3545c6ec8205f873b4246869515fc265f1f
IsmaelMase <ismaelmfb@gmail.com>
2019-07-16 23:37:11 +0200

    Cambios en los swagger

--- User-component/src/main/java/com/users/User/component/UserComponentApplication.java
+++ User-component/src/main/java/com/users/User/component/UserComponentApplication.java
@@ -3,15 +3,20 @@ package com.users.User.component;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController()
+@Api(tags = "zuul inside rest api")
 public class UserComponentApplication {
 
-	@RequestMapping("/home")
+	@GetMapping("/home")
+	@ApiOperation(value = "demo Example")
 	public String home() {
 		return "Hello world";
 	}
